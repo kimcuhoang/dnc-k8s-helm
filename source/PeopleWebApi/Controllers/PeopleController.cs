d4db8cb4dbd60468210c1aeccc0154de79495bff
kimcu-on-thenet <kim.cuhoang@gmail.com>
2020-02-20 11:22:54 +0700

    Initialize solution and project

---
+++ source/PeopleWebApi/Controllers/PeopleController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PeopleWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+
+        [HttpGet("ping")]
+        public IActionResult Ping() => Ok("pong");
+    }
+}
